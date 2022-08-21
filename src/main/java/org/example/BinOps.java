b769c16aca072a7a814282a3024a751aa0913e18
Artmeese <artmeese@gmail.com>
2022-08-21 23:02:20 +0500

    Initial Commit

---
+++ src/main/java/org/example/BinOps.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class BinOps {
+    public String sum(String a, String b) {
+        int binA = Integer.parseInt(a, 2);
+        int binB = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(binA + binB);
+    }
+
+    public String mult(String a, String b) {
+        int binA = Integer.parseInt(a, 2);
+        int binB = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(binA * binB);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bin = new BinOps();
+        System.out.println(bin.sum("10011", "100"));
+        System.out.println(bin.mult("11", "1011"));
+    }
+}
